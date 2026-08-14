7781e8dfadb3698527f6e7f35e7375992dcfbee1
Ethan Chuen <ethan.chuen@gmail.com>
2026-08-14 12:19:20 +1000

    clientside iso15118 communication

---
+++ EV/ev_client.py
@@ -0,0 +1,46 @@
+"""
+EV Client
+
+Placeholder methods for EV side communication
+with other testbed components.
+"""
+
+def discover_evse():
+    """Discover EV Charger (EVSE - Electric Vehicle Supply Equipment) on the network"""
+    pass
+
+def establish_tls_connection():
+    """Establish TLS connection to EVSE"""
+    pass
+
+def setup_communication_session():
+    """Setup communication session with EVSE"""
+    pass
+
+def discover_services():
+    """Discover available services on EVSE"""
+    pass
+
+def select_service():
+    """Select a specific service to use on EVSE"""
+    pass
+
+def authorize():
+    """    Authorize via Plug & Charge (PnC) or EVCC token exchange"""
+    pass
+
+def get_charge_parameters():
+    """Get charger parameters (max power, voltage limits)"""
+    pass
+
+def request_power_delivery():
+    """Start/Stop power delivery from EVSE"""
+    pass
+
+def get_meter_values():
+    """Get metering values (energy delivered, current, voltage)"""
+    pass
+
+def terminate_session():
+    """Terminate communication session with EVSE"""
+    pass
